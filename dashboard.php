66938da7bb91f3f40395c44bbb29c29fdcc83084
Justin Swanson <geeksunny@gmail.com>
2012-03-12 00:46:33 -0500

    Cleaned up the dashboard table a little bit.

--- dashboard.php
+++ dashboard.php
@@ -62,9 +62,8 @@ $links = $dbh->sqlQuery("SELECT * FROM `".$dbh->prefix."links` WHERE `userid`='"
 					<table width="100%" id="links_table" <?php if (!$links) echo 'style="display: none;"'; ?>>
 						<thead>
 							<tr>
-								<td class="round_topleft" style="width:5%;">#</td>
-								<td style="width:9%;">String</td>
-								<td style="width:80%;">URL</td>
+								<td class="round_topleft" style="width:9%;">String</td>
+								<td style="width:80%;">Target URL</td>
 								<td class="round_topright" style="width:6%; min-width: 65px; text-align: center;">Actions</td>
 							</tr>
 						</thead>
@@ -95,7 +94,7 @@ $links = $dbh->sqlQuery("SELECT * FROM `".$dbh->prefix."links` WHERE `userid`='"
 								}
 								$enabled = ($link['enabled'] == "1") ? "pause" : "play";
 								// TODO: Make row go gray or faded when the link is paused.
-								echo '<tr'.$row_class.'><td'.$left_class.'>'.$link['id'].'</td><td>'.$link['string'].'</td><td>'.$link['url'].'</td><td'.$right_class.' id="'.$link['id'].'"><img src="img/link.png" class="link" /><img src="img/stats.png" class="stats" /><img src="img/'.$enabled.'.png" class="toggle" /><img src="img/delete.png" class="delete" /></td></tr>';
+								echo '<tr'.$row_class.'><td'.$left_class.'>'.$link['string'].'</td><td>'.$link['url'].'</td><td'.$right_class.' id="'.$link['id'].'"><img src="img/link.png" class="link" /><img src="img/stats.png" class="stats" /><img src="img/'.$enabled.'.png" class="toggle" /><img src="img/delete.png" class="delete" /></td></tr>';
 								$zebra = !$zebra;	// Reversing the zebra stripe flag.
 								$i++;
 							}
